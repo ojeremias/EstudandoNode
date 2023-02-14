6105f81ca1f5c47a794cdd7d7ffe4461d15d3e73
ojeremias <jeremias123verissimo@gmail.com>
2023-02-14 17:06:40 -0300

    Acrescendo arquivo express

--- modules/express.js
+++ modules/express.js
@@ -7,6 +7,15 @@ app.get("/home", (req, res) => {
   res.status(200).send("<h1>Hello World</h1>");
 });
 
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "John Doe",
+      email: "John@doe.com",
+    },
+  ];
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Rodando com express na porta ${port}!`));
